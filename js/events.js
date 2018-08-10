4b6b44e5965b990d95314896cd14f2ea917025f4
Martin Cervantes <cervantes.martine@gmail.com>
2018-08-10 01:00:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+    $(getIt)
+    $(frameIt)
+    $(pressIt)
+    $(submitIt)
 });
+
+function getIt() {
+    $( "p" ).click(function() {
+        alert( "Hey!" );
+    });
+}
+
+function frameIt() {
+    $( "img" ).load(function() {
+        $("img").addClass("tasty");
+    });
+}
+
+function pressIt() {
+    $('form').on('keydown', function(key) {
+        if(key.which == 71){
+            alert('G was pressed');
+        }
+    });
+}
+
+function submitIt() {
+    $("form").submit(function(){
+        alert("Your form is going to be submitted now.");
+    });
+}
